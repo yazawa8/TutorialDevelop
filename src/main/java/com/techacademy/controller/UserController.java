8f39cc846edbef5d85da8c818d2dda71c89e18eb
yazawa8 <yaza_ta@yahoo.co.jp>
2024-04-21 14:28:38 +0900

    second-commit

---
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.techacademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PostMapping; // 追加
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.entity.User; // 追加
+import com.techacademy.service.UserService;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    /** 一覧画面を表示 */
+    @GetMapping("/list")
+    public String getList(Model model) {
+        // 全件検索結果をModelに登録
+        model.addAttribute("userlist", service.getUserList());
+        // user/list.htmlに画面遷移
+        return "user/list";
+    }
+
+    // ----- 追加:ここから -----
+    /** User登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+
+    /** User登録処理 */
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // ----- 追加:ここまで -----
+}
---
+++ src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,32 @@
+package com.techacademy.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
+
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository repository) {
+        this.userRepository = repository;
+    }
+
+    /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+
+    // ----- 追加:ここから -----
+    /** Userの登録を行なう */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
+
+}
