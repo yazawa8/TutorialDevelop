aff92efe2cb1499855f9a5df55d8f83a573b7f7c
yazawa8 <yaza_ta@yahoo.co.jp>
2024-04-21 14:32:46 +0900

    third-commit

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -2,7 +2,7 @@ package com.techacademy.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // 追加
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -22,11 +22,16 @@ public class UserService {
     }
 
     // ----- 追加:ここから -----
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    // ----- 追加:ここまで -----
+
+
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
-    // ----- 追加:ここまで -----
-
 }
